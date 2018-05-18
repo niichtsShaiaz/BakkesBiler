2b09e5ece3fcb08da5fb907d068be4aacc2c37f7
Jonatan Magnus Bakke <cph-jb281@cphbusiness.dk>
2018-05-18 07:41:47 +0200

    Added style to reservation page

--- Web-client/src/adminStuff/adminPageReservations.js
+++ Web-client/src/adminStuff/adminPageReservations.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react"
 
+import { Table } from 'reactstrap';
+
 const URL = "https://ezlinodev.com/cars/api/CarApi/";
 const TESTURL = "http://localhost:8084/jwtbackend/api/CarApi/reservations"
 
@@ -7,51 +9,64 @@ const TESTURL = "http://localhost:8084/jwtbackend/api/CarApi/reservations"
 function ListItem(props) {
     const reservation = props.reservation;
     return (
-      <li>
-        {reservation.customerMail}
-      </li>
+        <tr>
+            <td>{reservation.companyTag}</td>
+            <td>{reservation.customerMail}</td>
+            <td>{reservation.fromDate}</td>
+            <td>{reservation.toDate}</td>
+            <td>{reservation.vId}</td>
+        </tr>
     );
-  }
+}
 
 
 function ReservationList(props) {
     const reservations = props.reservations;
     const listItems = reservations.map((reservation, index) =>
-      <ListItem key={index} reservation={reservation}/>
+        <ListItem key={index} reservation={reservation} />
     );
     return (
-      <ul>{listItems}</ul>
-    );
-  }
+        <Table>
+            <thead>
+                <th>Company</th>
+                <th>Customer mail</th>
+                <th>From date</th>
+                <th>To date</th>
+                <th>Car id</th>
+            </thead>
+            <tbody>
+                {listItems}
+            </tbody>
+        </Table>
 
-class adminPageReservations extends Component 
-{
-    constructor(props){
+        
+    );
+}
+
+class adminPageReservations extends Component {
+    constructor(props) {
         super();
-        this.state = {reservations: []};
+        this.state = { reservations: [] };
     }
 
-    componentDidMount()
-    {
+    componentDidMount() {
         this.getReservationsFromApi();
     }
 
-    getReservationsFromApi()
-    {
+    getReservationsFromApi() {
         fetch(TESTURL)
-        .then(results => results.json())
-        .then(data => this.setState({reservations: data}))
+            .then(results => results.json())
+            .then(data => this.setState({ reservations: data }))
     }
 
-    
-    
-    render()
-    {
+
+
+    render() {
         console.log(this.state.reservations)
-        return(
+        return (
             <div>
                 <h1>Reservations</h1>
-                <ReservationList reservations={this.state.reservations}/>
+                <ReservationList reservations={this.state.reservations} />
             </div>
         )
     }
